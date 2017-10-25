e208add61cce8a75491e9c558a0fcbe80bb5a04f
martin <martin@within.co.kr>
2017-10-25 12:37:14 +0900

    Fix server module

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
-
-
 const express = require("express");
 const app = express();
-const port =4000;
+const port = 4000;
 const fs = require('fs');
 const bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded({extended:false}));
+app.use( bodyParser.urlencoded({extended:false}) );
 app.locals.pretty=true;
 app.set('views','./views');
-app.set('view engine' 'ejs');
+app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 //express.static('public')
 
-const server = app.use().listen(port, ()=>{
+const server = app.use(express).listen(port, ()=>{
     console.log(`Express Server has started on port : ${port}`)
 });
