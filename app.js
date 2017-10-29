856fc97c99ef1392fccb9989c8bb4282253a3c55
younghoon <hoons0131@gmail.com>
2017-10-29 23:21:38 +0900

    Update router for handlebars template

--- app.js
+++ app.js
@@ -2,22 +2,42 @@ const express = require("express");
 const app = express();
 const port = 4000;
 const fs = require('fs');
+const path = require('path');
 const bodyParser = require('body-parser');
+
 
 app.use( bodyParser.urlencoded({extended:false}) );
 app.locals.pretty=true;
-app.set('views','./views');
-app.set('view engine', 'ejs');
-app.engine('html', require('ejs').renderFile);
-//express.static('public')
 
 /**
- * @param { Router } 각 페이지별 Router 
+ * @param { handlebars template }
+ * https://github.com/ericf/express-handlebars
+ */
+const handlebars = require('express-handlebars');
+const hbs = handlebars.create({
+  defaultLayout: __dirname + '/src/views/index'
+});
+
+/**
+ * @param { views }: client views 설정
+ * 
+ */
+app.set('views',path.join(__dirname, 'src/views'));
+app.engine('handlebars', hbs.engine );
+app.set('view engine', 'handlebars');
+
+/**
+ * @param { Router }: 각 페이지별 Router 
  */
 const clientRouter = require('./router/client.router');
 const apiRouter = require('./router/api.router');
 clientRouter(app);
 apiRouter(app);
+
+/**
+ * @param static 파일 경로 설정
+ */
+app.use(express.static('./src/assets'));
 
 const server = app.use(express).listen(port, ()=>{
     console.log(`Express Server has started on port : ${port}`)
