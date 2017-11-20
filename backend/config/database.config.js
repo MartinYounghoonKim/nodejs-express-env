a098788dfa610dd2c7ec5cdd2505d68643004e77
martinyounghoonkim <hoons0131@gmail.com>
2017-11-20 23:04:00 +0900

    update config files

--- backend/config/database.config.js
+++ backend/config/database.config.js
@@ -1,9 +1,9 @@
 if(process.env.NODE_ENV === 'development'){
   module.exports ={
-    host : '127.0.0.1',
-    user : 'root',
-    password : 'root1',
-    port : 3306,
-    database : 'lazyside_test_DB'
+    host : '',
+    user : '',
+    password : '',
+    port : 0000,
+    database : ''
   };
 }
