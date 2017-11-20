5a95acfc2c268f0a931ac36dc0d0d693563f91ee
martinyounghoonkim <hoons0131@gmail.com>
2017-11-20 11:52:45 +0900

    Add node env constans

--- backend/config/database.config.js
+++ backend/config/database.config.js
@@ -1,7 +1,9 @@
-module.exports ={
-  host : '127.0.0.1',
-  user : 'root',
-  password : 'root1',
-  port : 3306,
-  database : 'lazyside_test_DB'
-};
+if(process.env.NODE_ENV === 'development'){
+  module.exports ={
+    host : '127.0.0.1',
+    user : 'root',
+    password : 'root1',
+    port : 3306,
+    database : 'lazyside_test_DB'
+  };
+}
